396e64251ea499a44492d89fcf57bacf71963110
Jiten Basnet <jiten.basnet@ebpearls.com>
2024-12-16 16:56:02 +0545

    :star: feat: add svg image

--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -4,4 +4,6 @@ abstract class AppColors {
   static const Color textLight = Color(0xFFCFCFCF);
 
   static const Color textDarkGrey = Color(0xFF57606A);
+
+  static const Color black = Color(0xFF000000);
 }
